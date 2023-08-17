ec7ffa9632f22aa1d62780bab969ada728118421
Victoria Kowalk <137898093+VictoriaKowalk@users.noreply.github.com>
2023-08-17 12:41:52 -0300

    Add files via upload

---
+++ Actividad 1 - Fechas/main.cpp
@@ -0,0 +1,18 @@
+/*Hacer una clase Fecha que permita generar objetos capaces de almacenar fechas y hacer operaciones con las mismas.
+La clase en ningún momento debe almacenar una fecha inválida. Es decir, una fecha que no exista en el calendario.
+Realizar la declaración de la clase en un archivo H y la definición de los métodos en un archivo CPP.
+La clase fecha debe almacenar los siguientes atributos enteros: _dia, _mes y _anio.
+
+*/
+#include <iostream>
+#include "Fecha.h"
+
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
+
