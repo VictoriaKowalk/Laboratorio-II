a10a0fa372ae7bf5d1f90fc689aca5d97a1be486
Victoria Kowalk <137898093+VictoriaKowalk@users.noreply.github.com>
2023-08-28 18:24:18 -0300

    Add files via upload

---
+++ BlackJack/Jugador.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include "Jugador.h"
+
+Jugador::Jugador( const char *nombre)
+{
+   _nombre = nombre;
+}
+Jugador::Jugador () {
+}
+
+Jugador::~Jugador()
+{
+    //dtor
+}
