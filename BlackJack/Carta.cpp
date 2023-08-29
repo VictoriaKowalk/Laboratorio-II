472b2d6487003ad158305fb7b0a86ef815e05305
Victoria Kowalk <137898093+VictoriaKowalk@users.noreply.github.com>
2023-08-29 13:04:56 -0300

    Add files via upload

---
+++ BlackJack/Carta.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Spanish");
+
+
+
+
+
+
+
+
+   system ("pause");
+   return 0;
+}
+#include "Carta.h"
+
+Carta::Carta()
+{
+    //ctor
+}
+
+Carta::~Carta()
+{
+    //dtor
+}
---
+++ BlackJack/Jugador.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Spanish");
+
+
+
+
+
+
+
+
+   system ("pause");
+   return 0;
+}
+#include "Jugador.h"
+
+Jugador::Jugador()
+{
+    //ctor
+}
+
+Jugador::~Jugador()
+{
+    //dtor
+}
